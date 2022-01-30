a75ef2705f0aca83153ec9bfcb1b18ba67f6238c
Aytaç <79279282+aytcp@users.noreply.github.com>
2022-01-30 14:59:42 +0300

    Create Covid19 Analysis in Turkey - Data.sql

---
+++ Covid19 Analysis in Turkey - Data.sql
@@ -0,0 +1,10 @@
+/*
+Covid 19 Data Exploration 
+
+The data taken from https://ourworldindata.org/covid-deaths.
+
+After the data set was received, data cleaning was performed to import it into the database. 
+
+The data was converted to appropriate data types, cleaned and imported into PostgreSQL for analysis in the appropriate format.
+
+*/
